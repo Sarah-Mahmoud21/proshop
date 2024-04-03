87145e52a958a9a00e703f33157614fba95c8902
Sarah_Mahmoud21 <saramahmoud15420@gmail.com>
2024-04-03 14:10:49 +0300

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { TextField, Button, Drawer } from "@mui/material";
+import "../Header/Header.css"; 
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import PersonIcon from "@mui/icons-material/Person";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+  return (
+    <div className="header">
+      <Button className="menu" onClick={toggleDrawer}><MenuIcon/></Button>
+      <h1>
+        <span>Pro</span>Shop
+      </h1>
+      <div className="search-div">
+        <TextField
+          id="outlined-basic"
+          label="search"
+          variant="outlined"
+        />
+        <Button variant="contained">Search</Button>
+      </div>
+      <div className="icons">
+        <ul>
+          <li><Button><PersonIcon /></Button></li>
+          <li><Button><BookmarkIcon /></Button></li>
+          <li><Button><ShoppingCartIcon /></Button></li>
+        </ul>          
+      </div>
+      <Drawer 
+        anchor="left" 
+        open={isDrawerOpen} 
+        onClose={toggleDrawer} 
+       >
+        <div className="drawer-content">
+          <ul  className="menu-bar" style={{ listStyleType: 'none', padding: 0 ,width: '100%'}}>
+            <li>
+            <div className="search-div">
+        <TextField
+          id="outlined-basic"
+          label="search"
+          variant="standard"
+        />
+        <Button  style={{color:'#FCDD06',border:'none' ,background:'none',boxShadow:'none'}} variant="contained"><SearchIcon/></Button>
+        </div>
+            </li>
+            <li><Button style={{color:'#FCDD06'}}><PersonIcon /></Button></li>
+            <li><Button style={{color:'#FCDD06'}}><BookmarkIcon /></Button></li>
+            <li><Button style={{color:'#FCDD06'}}><ShoppingCartIcon /></Button></li>
+          </ul>
+        </div>
+      </Drawer>
+    </div>
+  );
+}
+
+export default Header;
